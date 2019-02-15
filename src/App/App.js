3886078ca0a30928379a22b97f5372072072a192
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-02-15 09:29:39 -0500

    Clear out boilerplate, setup

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <p>IOU one App</p>
+    );
+  }
+}
+
+export default App;
