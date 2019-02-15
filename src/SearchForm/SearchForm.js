51b134d935ed58134653dda352982155b9b1efaa
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-02-15 12:24:39 -0500

    Connect search form to people search API

--- src/SearchForm/SearchForm.js
+++ src/SearchForm/SearchForm.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import search from '../search';
 import './SearchForm.css';
 
-function SearchForm (props) {
+class SearchForm extends React.Component {
 
-  return (
-    <p>IOU one search form</p>
-  );
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const term = e.target.elements['search-term'].value.trim();
+
+    if (term.length < 1) {
+      // TODO set error flag, do display in form
+      return;
+    }
+
+    search
+      .searchPeople(term)
+      .then((data) => {
+        this.props.onData({ people: data.results });
+      })
+      .catch((err) => {
+        this.props.onError(err);
+      })
+  }
+
+  render () {
+    return (
+      <form onSubmit={(e) => { this.handleFormSubmit(e) }}>
+        <label htmlFor="search-term">Search Term</label>
+        <input id="search-term" type="text"></input>
+        <button>Search</button>
+      </form>
+    );
+  }
 }
 
 SearchForm.defaultProps = {
