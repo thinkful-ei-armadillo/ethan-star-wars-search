66520db2cfa5261aaac93486acd81bc48b65ffe9
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-02-15 11:36:18 -0500

    Create SearchResultsList stub

---
+++ src/SearchResultsList/SearchResultsList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './SearchResultsList.css';
+
+function SearchResultsList (props) {
+
+  return (
+    <p>IOU one search results list</p>
+  );
+}
+
+export default SearchResultsList;
